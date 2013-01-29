99ad01121fc41d3536e9d9f20ef5c688c3342ad4
Bas <bassimhuis@gmail.com>
2013-01-29 07:36:15 +0100

    Created development branch

--- init.lua
+++ init.lua
@@ -21,17 +21,6 @@ local add_plant = function(name_plant) -- register a wild plant
   })
 end
 
-minetest.register_alias("harvest:lavender_wild", "plants:lavender_wild")
-minetest.register_alias("harvest:redshroom_wild", "plants:redshroom_wild")
-minetest.register_alias("harvest:corn_wild", "plants:corn_wild")
-minetest.register_alias("harvest:cotton_wild", "plants:cotton_wild")
-minetest.register_alias("harvest:brownshroom_wild", "plants:brownshroom_wild")
-minetest.register_alias("harvest:chamomile_wild", "plants:chamomile_wild")
-minetest.register_alias("harvest:colchicum_wild", "plants:colchicum_wild")
-minetest.register_alias("harvest:poppy_wild", "plants:poppy_wild")
-minetest.register_alias("harvest:grasstall_wild", "plants:grasstall_wild")
-minetest.register_alias("harvest:grass_wild", "plants:grass_wild")
-
 add_plant("cotton")
 add_plant("corn")
 add_plant("lavender")
@@ -45,15 +34,138 @@ add_plant("poppy")
 add_plant("grasstall")
 add_plant("grass")
 
-habitat:generate("plants:lavender_wild", {"default:dirt_with_grass"}, minp, maxp, 2, 60, 4, 4, {"default:sand"},0,{""})
-habitat:generate("plants:redshroom_wild", {"default:dirt_with_grass"}, minp, maxp, 2, 60, 20, 8, {"default:leaves"},0,{""})
-habitat:generate("plants:corn_wild", {"default:dirt_with_grass"}, minp, maxp, 2, 60, 8, 10, {"default:water_source"},0,{""})
-habitat:generate("plants:cotton_wild", {"default:dirt_with_grass"}, minp, maxp, 2, 60, 8, 10, {"default:desert_sand"},0,{""})
-habitat:generate("plants:brownshroom_wild", {"default:stone"}, minp, maxp, -40, 2, 2, 10, {"default:water_source"},0,{""})
-habitat:generate("plants:chamomile_wild", {"default:dirt_with_grass"}, minp, maxp, 2, 40, 8, 4, {"default:stone_with_coal"},0,{""})
-habitat:generate("plants:colchicum_wild", {"default:dirt_with_grass"}, minp, maxp, 2, 40, 4, 4, {"default:stone_with_iron"},0,{""})
-habitat:generate("plants:poppy_wild", {"defaultw:desert_sand"}, minp, maxp, 2, 20, 4, 10, {"default:water_source"},0,{""})
-habitat:generate("plants:grasstall_wild", {"default:dirt_with_grass"}, minp, maxp, 2, 20, 3, 3, {"default:water_source"},0,{""})
-habitat:generate("plants:grass_wild", {"default:dirt_with_grass"}, minp, maxp, 2, 20, 3, 3, {"default:water_source"},0,{""})
+
+plantslib:register_generate_plant({
+    surface = "default:dirt_with_grass",
+    near_nodes = {"default:sand"},
+    near_nodes_size = 4,
+    near_nodes_vertical = 4,
+    min_elevation = 2,
+    max_elevation = 60,
+    rarity = 8,
+    plantlife_limit = -0.9,
+  }, 
+  "plants:lavender_wild"
+)
+
+
+plantslib:register_generate_plant({
+    surface = "default:dirt_with_grass",
+    near_nodes = {"default:leaves"},
+    near_nodes_size = 8,
+    near_nodes_vertical = 8,
+    min_elevation = 2,
+    max_elevation = 60,
+    rarity = 10,
+    plantlife_limit = -0.8,
+  }, 
+  "plants:redshroom_wild"
+)
+
+
+plantslib:register_generate_plant({
+    surface = "default:dirt_with_grass",
+    near_nodes = {"default:water_source"},
+    near_nodes_size = 10,
+    near_nodes_vertical = 10,
+    min_elevation = 2,
+    max_elevation = 60,
+    rarity = 5,
+    plantlife_limit = -1,
+  }, 
+  "plants:corn_wild"
+)
+
+plantslib:register_generate_plant({
+    surface = "default:dirt_with_grass",
+    near_nodes = {"default:desert_sand"},
+    near_nodes_size = 10,
+    near_nodes_vertical = 10,
+    min_elevation = 2,
+    max_elevation = 60,
+    rarity = 8,
+    plantlife_limit = -0.5,
+  }, 
+  "plants:cotton_wild"
+)
+
+plantslib:register_generate_plant({
+    surface = "default:stone",
+    near_nodes = {"default:water_source"},
+    near_nodes_size = 10,
+    near_nodes_vertical = 10,
+    min_elevation = -40,
+    max_elevation = 2,
+    rarity = 2,
+    plantlife_limit = -0.7,
+  }, 
+  "plants:brownshroom_wild"
+)
+
+
+
+plantslib:register_generate_plant({
+    surface = "default:dirt_with_grass",
+    near_nodes = {"default:stone_with_coal"},
+    near_nodes_size = 4,
+    near_nodes_vertical = 4,
+    min_elevation = 2,
+    max_elevation = 40,
+    rarity = 8,
+  }, 
+  "plants:chamomile_wild"
+)
+
+plantslib:register_generate_plant({
+    surface = "default:dirt_with_grass",
+    near_nodes = {"default:stone_with_iron"},
+    near_nodes_size = 4,
+    near_nodes_vertical = 4,
+    min_elevation = 2,
+    max_elevation = 40,
+    rarity = 4,
+  }, 
+  "plants:colchicum_wild"
+)
+
+plantslib:register_generate_plant({
+    surface = "default:desert_sand",
+    near_nodes = {"default:water_source"},
+    near_nodes_size = 10,
+    near_nodes_vertical = 10,
+    min_elevation = 2,
+    max_elevation = 40,
+    rarity = 8,
+  }, 
+  "plants:poppy_wild"
+)
+
+plantslib:register_generate_plant({
+    surface = "default:dirt_with_grass",
+    near_nodes = {"default:water_source"},
+    near_nodes_size = 8,
+    near_nodes_vertical = 8,
+    near_nodes_count = 2,
+    min_elevation = 2,
+    max_elevation = 40,
+    rarity = 1,
+    plantlife_limit = -1,
+  }, 
+  "plants:grasstall_wild"
+)
+
+plantslib:register_generate_plant({
+    surface = "default:dirt_with_grass",
+    near_nodes = {"default:water_source"},
+    near_nodes_size = 16,
+    near_nodes_vertical = 16,
+    near_nodes_count = 2,
+    min_elevation = 2,
+    max_elevation = 60,
+    rarity = 2,
+    plantlife_limit = -1,
+  }, 
+  "plants:grass_wild"
+)
 
 print("[Plants] Loaded!")
